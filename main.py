aff120ce1bd442cb7b2fe190e65dbf33fc9a2838
caiquanshi <xandercai666@gmail.com>
2022-11-01 00:38:02 -0400

    transfer learning part for resnet done

--- main.py
+++ main.py
@@ -7,17 +7,31 @@ import shutil
 IMAGE_SIZE = (224, 224)
 IMAGE_PATH = "./Project_A_Supp/mhist_dataset/images/"
 ANNOTATION_PATH = "./Project_A_Supp/mhist_dataset/annotations.csv"
-IMAGE_PATH_WITH_LABEL = "./images_with_label/"
+IMAGE_PATH_WITH_VOTE = "./images_with_label_vote"
+IMAGE_PATH_WITH_MAJORITY = "./images_with_label_majority"
 IMAGE_SHAPE = (224, 224, 3)
-NUM_CLASSES = 8
+NUM_CLASSES = 2
 
 
-def file_generator(mhist_dir, new_dir, annotation_path=ANNOTATION_PATH):
+def file_generator_by_vote(mhist_dir, new_dir, annotation_path=ANNOTATION_PATH):
     annotation = pd.read_csv(annotation_path)
     for index, line in annotation.iterrows():
         original = mhist_dir + "{}".format(line["Image Name"])
         target = new_dir + "{}/{}/". \
             format(line["Partition"], line["Number of Annotators who Selected SSA (Out of 7)"])
+        try:
+            shutil.copy(original, target)
+        except IOError as io_err:
+            os.makedirs(os.path.dirname(target))
+            shutil.copy(original, target)
+
+
+def file_generator_by_majority(mhist_dir, new_dir, annotation_path=ANNOTATION_PATH):
+    annotation = pd.read_csv(annotation_path)
+    for index, line in annotation.iterrows():
+        original = mhist_dir + "{}".format(line["Image Name"])
+        target = new_dir + "{}/{}/". \
+            format(line["Partition"], line["Majority Vote Label"])
         try:
             shutil.copy(original, target)
         except IOError as io_err:
@@ -58,17 +72,21 @@ def data_loader(image_dir):
     return train_ds, val_ds, test_ds
 
 
-def create_pretrain_resnet_model():
+def create_pretrain_resnet_model(ft=0):
     res_net_model = tf.keras.applications.resnet_v2.ResNet50V2(include_top=False, weights='imagenet',
                                                                input_shape=IMAGE_SHAPE)
-    for layer in res_net_model.layers:
-        layer.trainable = False
-    model = tf.keras.Sequential()
-    model.add(res_net_model)
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
-    model.summary()
-    return model
+
+    for i in range(len(res_net_model.layers)-ft):
+        res_net_model.layers[i].trainable = False
+
+    # classifier = tf.keras.layers.Conv2D(128, (3, 3), activation='relu')(res_net_model.output)
+    # classifier = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(classifier)
+    classifier = tf.keras.layers.Flatten()(res_net_model.output)
+    # classifier = tf.keras.layers.Dense(100, activation='relu')(classifier)
+    classifier = tf.keras.layers.Dense(NUM_CLASSES)(classifier)
+    re_model = tf.keras.models.Model(inputs=res_net_model.input, outputs=classifier)
+    re_model.summary()
+    return re_model
 
 def compute_num_correct(model, images, labels):
   """Compute number of correctly classified images in a batch.
@@ -137,8 +155,9 @@ def compute_loss_fun(model, images, labels):
 
 
 if __name__ == "__main__":
-    print(device_lib.list_local_devices())
-    # file_generator(IMAGE_PATH, IMAGE_PATH_WITH_LABEL)
-    train_ds, val_ds, test_ds = data_loader(IMAGE_PATH_WITH_LABEL)
-    res_net = create_pretrain_resnet_model()
+    # tf.debugging.set_log_device_placement(True)
+    # file_generator_by_vote(IMAGE_PATH, IMAGE_PATH_WITH_LABEL)
+    file_generator_by_majority(IMAGE_PATH, IMAGE_PATH_WITH_MAJORITY)
+    train_ds, val_ds, test_ds = data_loader(IMAGE_PATH_WITH_MAJORITY)
+    res_net = create_pretrain_resnet_model(10)
     train_and_eveluate(res_net, train_ds, test_ds, 12, compute_loss_fun)
